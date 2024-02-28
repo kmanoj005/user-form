31ecd1b71533cde7058154876304c56900078cd9
Manoj <maanojkumar18@gmail.com>
2024-02-28 17:18:21 +0530

    second commit

--- src/pages/UserData.jsx
+++ src/pages/UserData.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 
 export default function UserData() {
-  return (
-    <div>
-      <div class="relative max-w-[1320px] mx-auto overflow-x-auto shadow-md sm:rounded-lg mt-28">
+    let [users, setUsers] = useState([]);
+
+    let getUserData = () => {
+        axios.get('https://raipradeep.in/form-api/viewUser.php')
+            .then((res) => res.data)
+            .then((finalRes) => {
+                setUsers(finalRes.dataList)
+            })
+    }
+
+    useEffect(() => {
+        getUserData()
+    }, [])
+
+    return (
+        <div>
+            <div class="relative max-w-[1320px] mx-auto overflow-x-auto shadow-md sm:rounded-lg mt-28">
                 <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                     <thead class="text-xs uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-100">
                         <tr>
@@ -28,73 +44,47 @@ export default function UserData() {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                               1320
-                            </th>
-                            <td class="px-6 py-4">
-                                Dev
-                            </td>
-                            <td class="px-6 py-4">
-                                sdsds@gmail.com
-                            </td>
-                            <td class="px-6 py-4">
-                                515261233
-                            </td>
-                            <td class="px-6 py-4" type="password" >
-                                52125
-                            </td>
-                            
-                            <td class="px-6 py-4 ">
-                                <button type="button" className=' pr-8 '>Edit</button>
-                                <button type="button">Delete</button>
-                            </td>
-                        </tr>
-                        <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                              4532
-                            </th>
-                            <td class="px-6 py-4">
-                                Veer
-                            </td>
-                            <td class="px-6 py-4">
-                                llklk@gmail.com
-                            </td>
-                            <td class="px-6 py-4">
-                                568123222
-                            </td>
-                            <td class="px-6 py-4">
-                                51333
-                            </td>
-                            <td class="px-6 py-4 ">
-                                <button type="button" className=' pr-8 '>Edit</button>
-                                <button type="button">Delete</button>
-                            </td>
-                        </tr>
-                        <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                               8521
-                            </th>
-                            <td class="px-6 py-4">
-                                Raj
-                            </td>
-                            <td class="px-6 py-4">
-                                vvdvdv@gmail.com
-                            </td>
-                            <td class="px-6 py-4">
-                                6512332154
-                            </td>
-                            <td class="px-6 py-4">
-                                8456
-                            </td>
-                            <td class="px-6 py-4 ">
-                                <button type="button" className=' pr-8 '>Edit</button>
-                                <button type="button">Delete</button>
-                            </td>
-                        </tr> 
+
+                        {
+                            users.length > 1 ?
+                                users.map((v, i) => {
+                                    return (
+                                        <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                {i + 1}
+                                            </th>
+                                            <td class="px-6 py-4">
+                                                {v.en_name}
+                                            </td>
+                                            <td class="px-6 py-4">
+                                                {v.en_email}
+                                            </td>
+                                            <td class="px-6 py-4">
+                                                {v.en_contact}
+                                            </td>
+                                            <td class="px-6 py-4" type="password" >
+                                                {v.en_password}
+                                            </td>
+                                            <td class="px-6 py-4 ">
+                                                <button type="button" className=' pr-8 '>Edit</button>
+                                                <button type="button">Delete</button>
+                                            </td>
+                                        </tr>
+                                    )
+                                })
+                                :
+                                <tr>
+                                    <td colSpan={5}> Data not found.</td>
+                                </tr>
+                        }
+
+
+
                     </tbody>
                 </table>
+                <Link to={'/'}><button class="text-white mx-auto flex justify-center mt-10 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Back to Login page.</button></Link>
+
             </div>
-    </div>
-  )
+        </div>
+    )
 }
--- src/pages/UserForm.jsx
+++ src/pages/UserForm.jsx
@@ -1,31 +1,63 @@
-import React from 'react'
+import axios, { toFormData } from 'axios'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 export default function UserForm() {
+
+    let navigate=useNavigate();
+
+    let [userData, setuserData] = useState({
+        name: '',
+        email: '',
+        mobile: '',
+        password: ''
+    })
+
+    let changeValue = (event) => {
+        let obj = { ...userData }
+        obj[event.target.name] = event.target.value;
+        setuserData(obj)
+    }
+
+    let saveData = (event) => {
+        event.preventDefault();
+        axios.post('https://raipradeep.in/form-api/saveUser.php', toFormData(userData))
+            .then((res) => res.data)
+            .then((finalRes) => {
+                setuserData({
+                    name: '',
+                    email: '',
+                    mobile: '',
+                    password: ''
+                })
+                navigate('/user-data');
+            })
+    }
+
     return (
         <div>
-
-            <div class="max-w-[450px] mx-auto my-44 py-10 bg-teal-100 px-8 rounded-lg">
+            <form onSubmit={saveData} class="max-w-[450px] mx-auto my-28 py-10 bg-teal-100 px-8 rounded-lg">
                 <div class="relative z-0 w-full mb-5 group">
-                    <input type="text" name="floating_first_name" id="floating_first_name" class="block py-2.5 px-0 w-full text-md text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                    <input type="text" name="name" value={userData.name} onChange={changeValue} id="floating_first_name" class="block py-2.5 px-0 w-full text-md text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                     <label for="floating_first_name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Name</label>
                 </div>
 
                 <div class="relative z-0 w-full mb-5 group">
-                    <input type="email" name="floating_email" id="floating_email" class="block py-2.5 px-0 w-full text-md text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                    <input type="email" name="email" value={userData.email} onChange={changeValue} id="floating_email" class="block py-2.5 px-0 w-full text-md text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                     <label for="floating_email" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
                 </div>
                 <div class="relative z-0 w-full mb-5 group">
-                    <input type="tel"  name="floating_phone" id="floating_phone" class="block py-2.5 px-0 w-full text-md text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                    <input type="tel" name="mobile" value={userData.mobile} onChange={changeValue} id="floating_phone" class="block py-2.5 px-0 w-full text-md text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                     <label for="floating_phone" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Phone number</label>
                 </div>
                 <div class="relative z-0 w-full mb-5 group">
-                    <input type="password" name="floating_password" id="floating_password" class="block py-2.5 px-0 w-full text-md text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                    <input type="password" name="password" onChange={changeValue} value={userData.password} id="floating_password" class="block py-2.5 px-0 w-full text-md text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                     <label for="floating_password" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
                 </div>
-                        
-                <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
-            </div>
 
+                <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button> <br />
+                <Link to={'/user-data'}><button class="text-white mx-auto flex justify-center mt-10 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Go to User-Data page.</button></Link>
+            </form>
         </div>
     )
 }
